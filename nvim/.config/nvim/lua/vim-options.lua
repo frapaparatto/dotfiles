875a50ef9b9c05bb578bc5be82dd88ee9eaed608
Francesco Paparatto <francescopaparatto@gmail.com>
2026-08-17 23:06:01 +0200

    Add visual line move and centered scroll keymaps

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -58,3 +58,9 @@ end
 -- Insert mode escape
 vim.keymap.set("i", "jk", "<ESC>")
 vim.o.timeoutlen = 250
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "C-d", "<C-d>zz")
+vim.keymap.set("n", "C-u", "<C-u>zz")
