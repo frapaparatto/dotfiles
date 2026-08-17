356094ebb7fbf3b1263c28a02334c0d330d659bb
Francesco Paparatto <francescopaparatto@gmail.com>
2026-08-17 22:26:44 +0200

    Make telescope find_files and live_grep search hidden files, excluding .git

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,25 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("telescope").setup({
+				defaults = {
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"--hidden",
+						"--glob=!.git/*",
+					},
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+						file_ignore_patterns = { "%.git/" },
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
