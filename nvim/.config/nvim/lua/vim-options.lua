566bbfa1cd6078bf84b167fcfdaf8eadb195dd2a
Francesco Paparatto <francescopaparatto@gmail.com>
2026-08-17 23:06:31 +0200

    Fix C-d/C-u keymap lhs to use angle-bracket notation

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -62,5 +62,5 @@ vim.o.timeoutlen = 250
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "C-d", "<C-d>zz")
-vim.keymap.set("n", "C-u", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
